0f4dba29f5e8b120b6ad7517dace6368760b911b
Andrew Green <a.f.green1@gmail.com>
2022-02-19 14:48:37 +0000

    Create decorators for key types
    Used in conjunction with hasattr to automagically colour code things!

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import time
-
+# import functools
 
 
 def sleep(seconds):
@@ -11,3 +11,40 @@ def sleep(seconds):
     while elapsed < seconds:
         elapsed = time.monotonic() - now
 
+
+class launcher():
+    def __init__(self, func):
+        self.func = func
+        self.is_launcher = True
+
+    def __call__(self, *args, **kwargs):
+        return self.func()
+
+
+
+class emergency_exit():
+    def __init__(self, func):
+        self.func = func
+        self.is_exit = True
+    
+    def __call__(self, *args, **kwargs):
+
+        return self.func()
+    
+
+class macro():
+    def __init__(self, func):
+        self.func = func
+        self.is_macro = True
+    
+    def __call__(self, *args, **kwargs):
+        return self.func()
+    
+class noop():
+    def __init__(self, func):
+        self.func = func
+        self.is_noop = True
+    
+    def __call__(self, *args, **kwargs):
+        return self.func()
+    
